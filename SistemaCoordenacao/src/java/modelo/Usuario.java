66f28d9f298816cab91d025c999d9032aee8a66b
pauladbol <pauladbol@hotmail.com>
2016-04-22 16:06:19 -0300

    adicionando herança a classe usuário

--- SistemaCoordenacao/src/java/modelo/Usuario.java
+++ SistemaCoordenacao/src/java/modelo/Usuario.java
@@ -4,15 +4,27 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="usuario")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue
+    private int id;
     private int matricula;
     private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public int getMatricula() {
         return matricula;
