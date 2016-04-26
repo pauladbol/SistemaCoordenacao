64135b3c06e0925bbfd2b2891d507efdcd658a5f
10070128 <10070128@LAB411-22-39724.restinga.ifrs.edu.br>
2016-04-26 20:19:08 -0300

    -Ajuste de layout -Criado tela para abrir periodo de solicitacao

---
+++ SistemaCoordenacao/src/java/modelo/PeriodoSolicitacao.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import persistencia.PeriodoSolicitacaoDAO;
+
+/**
+ *
+ * @author 10070128
+ */
+@Entity
+@Table(name="periodosolicitacao")
+public class PeriodoSolicitacao implements Serializable {
+    
+    @Id
+    @GeneratedValue
+    private int id;
+    
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Column(name="data_inicio")
+    private Date dataInicio;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Column(name="data_fim")
+    private Date dataFim;
+    private boolean estado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    public static void main(String[] args) {
+        PeriodoSolicitacaoDAO dao = new PeriodoSolicitacaoDAO();
+        PeriodoSolicitacao periodo = new PeriodoSolicitacao();
+        
+        periodo.setDataInicio(new Date());
+        periodo.setDataFim(new Date());
+        periodo.setEstado(true);
+        dao.criar(periodo);
+        dao.encerrarSessao();
+    }
+}
---
+++ SistemaCoordenacao/src/java/persistencia/PeriodoSolicitacaoDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import modelo.PeriodoSolicitacao;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author 10070128
+ */
+public class PeriodoSolicitacaoDAO {
+    
+private final Session sessao;
+    
+    public PeriodoSolicitacaoDAO() {
+        sessao = HibernateUtil.getSessionFactory().getCurrentSession();
+        sessao.beginTransaction();
+    }
+    
+    public PeriodoSolicitacao buscar() {
+        return (PeriodoSolicitacao) sessao
+            .createQuery("select * from periodosolicitacao where estado = 'A'").uniqueResult();
+    }
+    
+    public void criar(PeriodoSolicitacao p){
+        sessao.save(p);
+    }
+    
+    public void encerrarSessao() {
+        sessao.close();
+    }
+//create periodo solicitacao
+    
+    //find periodo solicitacao
+}
