77c5b697d7c2d4316459a1015fbb1fb68010cf24
BrunoScheltzke <brunofonten@gmail.com>
2016-05-02 16:20:41 -0300

    Layout Adicionado

--- SistemaCoordenacao/src/java/persistencia/UsuarioDAO.java
+++ SistemaCoordenacao/src/java/persistencia/UsuarioDAO.java
@@ -2,14 +2,28 @@ package persistencia;
 
 import modelo.Usuario;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 public class UsuarioDAO {
     private Session sessao;
     
     public UsuarioDAO() {
-        sessao = HibernateUtil.getSessionFactory().getCurrentSession();
-        sessao.beginTransaction();
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        
+        Transaction tx = null;
+        try {
+            tx = sessao.beginTransaction();
+            tx.commit();
+        }
+        catch (Exception e) {
+        if (tx!=null) tx.rollback();
+            throw e;
+        }
+        finally {
+            //sessao.close();
+        }
+ 
     }
     
     public Usuario carregar(int matricula) {
